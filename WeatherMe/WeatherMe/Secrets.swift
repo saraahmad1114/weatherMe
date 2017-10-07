514532941dee97b75af2b6a52308b768218f00e8
Sara <saraahmad1114@yahoo.com>
2017-10-07 17:27:01 -0400

    checking

---
+++ WeatherMe/WeatherMe/Secrets.swift
@@ -0,0 +1,15 @@
+//
+//  Secrets.swift
+//  WeatherMe
+//
+//  Created by Flatiron School on 10/7/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    
+    static let darkSkyApiKey = "e73f5a6ee5fa277ced4af79f4686747d"
+    
+}
