e47c184a72f451cd4a12879eaa9bd6ed01a3ffd1
Sara <saraahmad1114@yahoo.com>
2017-10-09 17:05:43 -0400

    created the HourlyWeather file

---
+++ WeatherMe/WeatherMe/HourlyWeather.swift
@@ -0,0 +1,16 @@
+//
+//  HourlyWeather.swift
+//  WeatherMe
+//
+//  Created by Flatiron School on 10/9/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class HourlyWeather {
+    
+    
+    
+    
+}
