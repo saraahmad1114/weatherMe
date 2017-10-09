d5d352d1533c0def518c6311ca4ddf16e91c3a95
Sara <saraahmad1114@yahoo.com>
2017-10-09 17:03:57 -0400

    defined the CurrentWeather class

---
+++ WeatherMe/WeatherMe/CurrentWeather.swift
@@ -0,0 +1,34 @@
+//
+//  CurrentWeather.swift
+//  WeatherMe
+//
+//  Created by Flatiron School on 10/9/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class CurrentWeather {
+    
+    var time: Double
+    var summary: String
+    var icon: String
+    var precipitationProbability: Double
+    var temperature: Double
+    var humidity: Double
+    var cloudCover: Double
+    var timeZone: String
+    
+    init(time: Double, summary: String, icon: String, precipitationProbability: Double, temperature: Double, humidity: Double, cloudCover: Double, timeZone: String) {
+        self.time = time
+        self.summary = summary
+        self.icon = icon
+        self.precipitationProbability = precipitationProbability
+        self.temperature = temperature
+        self.humidity = humidity
+        self.cloudCover = cloudCover
+        self.timeZone = string
+    }
+    
+    
+}
