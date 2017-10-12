15b84a239fa3bd5b67f599ae493f9bfe0190da69
Sara <saraahmad1114@yahoo.com>
2017-10-12 16:50:07 -0400

    GoogleCoordinateAPIClient

---
+++ WeatherMe/WeatherMe/GoogleCoordinateAPIClient.swift
@@ -0,0 +1,18 @@
+//
+//  GoogleCoordinateAPIClient.swift
+//  WeatherMe
+//
+//  Created by Flatiron School on 10/12/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class GoogleCoordinateAPIClient {
+    
+    
+    
+    
+    
+    
+}
