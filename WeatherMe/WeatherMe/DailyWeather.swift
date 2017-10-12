11266e6ba4d23c5016cc6fa714ebfe2ce1fa5601
Sara <saraahmad1114@yahoo.com>
2017-10-12 00:24:03 -0400

    added dailyWeather

--- WeatherMe/WeatherMe/DailyWeather.swift
+++ WeatherMe/WeatherMe/DailyWeather.swift
@@ -8,9 +8,31 @@
 
 import Foundation
 
-class DailyWeather{
+class DailyWeather {
     
+    var dailyTopSummary: String
+    var dailyTopIcon: String
+    var dailyTime: Double
+    var dailyBottomSummary: String
+    var dailyBottomIcon: String
+    var dailyPrecipProbability: Double
+    var dailyTemperatureHigh: Double
+    var dailyTemperatureLow: Double
+    var dailyHumidity: Double
+    var dailyCloudCover: Double
     
-    
+    init(dailyTopSummary: String, dailyTopIcon: String, dailyTime: Double, dailyBottomSummary: String, dailyBottomIcon: String, dailyPrecipProbability: Double, dailyTemperatureHigh: Double, dailyTemperatureLow: Double, dailyHumidity: Double, dailyCloudCover: Double) {
+        
+        self.dailyTopSummary = dailyTopSummary
+        self.dailyTopIcon = dailyTopIcon
+        self.dailyTime = dailyTime
+        self.dailyBottomSummary = dailyBottomSummary
+        self.dailyBottomIcon = dailyBottomIcon
+        self.dailyPrecipProbability = dailyPrecipProbability
+        self.dailyTemperatureHigh = dailyTemperatureHigh
+        self.dailyTemperatureLow = dailyTemperatureLow
+        self.dailyHumidity = dailyHumidity
+        self.dailyCloudCover = dailyCloudCover
+    }
     
 }
