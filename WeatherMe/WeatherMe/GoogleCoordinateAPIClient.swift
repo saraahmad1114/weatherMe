7bf9236ed75862f8ddd34e5b862453035af62970
Sara <saraahmad1114@yahoo.com>
2017-10-13 23:05:47 -0400

    finished getCoordinateInformation function

--- WeatherMe/WeatherMe/GoogleCoordinateAPIClient.swift
+++ WeatherMe/WeatherMe/GoogleCoordinateAPIClient.swift
@@ -10,16 +10,31 @@ import Foundation
 
 class GoogleCoordinateAPIClient {
     
-    class func getCoordinateInformation () -> (){
+    class func getCoordinateInformation (zipCode: String, completion:@escaping([String: Any])->Void){
         
+        var jsonDictionary = [String: Any]()
         
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=\(zipCode)+CA&key=\(Secrets.googleCoordinateApiKey)"
         
+        let convertedUrl = URL(string: url)
         
+        guard let unwrappedConvertedUrl = convertedUrl else {print("unwrappedConvertedUrl did not unwrap"); return}
         
+        let request = URLRequest(url: unwrappedConvertedUrl)
         
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let unwrappedData = data else {print("unwrappedData did not unwrap"); return}
+            
+            let json = try? JSONSerialization.jsonObject(with: unwrappedData, options: []) as? [String: Any]
+            
+            guard let unwrappedJson = json else {print("unwrappedJson did not unwrap"); return }
+            
+            jsonDictionary = unwrappedJson
+            
+            completion(jsonDictionary)
+        }
+        task.resume()
     }
-    
-    
-    
     
 }
