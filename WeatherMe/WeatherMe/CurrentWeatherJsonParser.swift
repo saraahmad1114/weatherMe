b1db763b5ee6de0fcfb22584205451781bac939e
Sara <saraahmad1114@yahoo.com>
2017-10-15 22:28:34 -0400

    CurrentWeatherJsonParser

---
+++ WeatherMe/WeatherMe/CurrentWeatherJsonParser.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeatherJsonParser.swift
+//  WeatherMe
+//
+//  Created by Flatiron School on 10/15/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class CurrentWeatherJsonParser {
+    
+    
+    
+    
+    
+}
---
+++ WeatherMe/WeatherMe/GoogleCoordinateAPIJsonParser.swift
@@ -0,0 +1,15 @@
+//
+//  GoogleCoordinateAPIJsonParser.swift
+//  WeatherMe
+//
+//  Created by Flatiron School on 10/13/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class GoogleCoordinateAPIJsonParser{
+    
+    
+    
+}
