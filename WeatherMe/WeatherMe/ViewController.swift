a20e506a9045cea0eec108f544fb94caff1c940f
saraahmad1114 <asara1114@gmail.com>
2018-01-01 17:49:14 -0500

    changes

--- WeatherMe/WeatherMe/ViewController.swift
+++ WeatherMe/WeatherMe/ViewController.swift
@@ -11,20 +11,33 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate{
     
-    var locationManager = CLLocationManager()
+    var locationManager: CLLocationManager!
     var currentLocation: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-      
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
     }
+    
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        if status == .authorizedWhenInUse {
+//            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+//                if CLLocationManager.isRangingAvailable() {
+//                  
+//                }
+//            }
+//        }
 
-    override func didReceiveMemoryWarning() {
+
+
+        func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    }
 }
 
