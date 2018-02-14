2aa24761d86b9b62ed29557861acfa758c1d1b15
saraahmad1114 <asara1114@gmail.com>
2018-02-13 21:07:04 -0500

    created uielements

--- WeatherMe/WeatherMe/ViewController.swift
+++ WeatherMe/WeatherMe/ViewController.swift
@@ -18,12 +18,7 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         createGradientLayer()
-        
-        let placeholder = NSAttributedString(string: "Enter ZipCode Here", attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightGray])
-        self.zipCodeTextField.attributedPlaceholder = placeholder
-        self.zipCodeTextField.textColor = UIColor.gray
-        self.zipCodeTextField.borderStyle = UITextBorderStyle.roundedRect
-        self.zipCodeTextField.clearsOnBeginEditing = true
+        createCustomTextField(textfield: self.zipCodeTextField)
     }
     
     func createGradientLayer() {
@@ -31,6 +26,14 @@ class ViewController: UIViewController{
         self.gradientLayer.frame = self.view.bounds
         self.gradientLayer.colors = [UIColor.white.cgColor, UIColor.cyan.cgColor]
         self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    func createCustomTextField (textfield: UITextField){
+          let placeholder = NSAttributedString(string: "Enter ZipCode Here", attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightGray])
+          textfield.attributedPlaceholder = placeholder
+          textfield.textColor = UIColor.gray
+          textfield.borderStyle = UITextBorderStyle.roundedRect
+          textfield.clearsOnBeginEditing = true
     }
 
     override func viewWillAppear(_ animated: Bool) {
