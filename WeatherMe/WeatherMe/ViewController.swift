a38e21e33abdcec1bc969198eec5fa7c5b1ffe59
saraahmad1114 <asara1114@gmail.com>
2018-02-13 22:44:56 -0500

    uialertviewcontroller done

--- WeatherMe/WeatherMe/ViewController.swift
+++ WeatherMe/WeatherMe/ViewController.swift
@@ -14,11 +14,14 @@ class ViewController: UIViewController{
     var gradientLayer: CAGradientLayer!
     @IBOutlet weak var insertZipcodeLabel: UILabel!
     @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var checkZipCodeButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         createGradientLayer()
         createCustomTextField(textfield: self.zipCodeTextField)
+        self.checkZipCodeButton.tintColor = UIColor.gray
     }
     
     func createGradientLayer() {
@@ -44,6 +47,30 @@ class ViewController: UIViewController{
         super.viewWillAppear(animated)
         createCustomLabel(label: self.insertZipcodeLabel)
     }
+    
+    func isZipCodeValid(text: String) -> Bool
+    {
+        let zipCodeTestPredicate = NSPredicate (format:"SELF MATCHES %@","(^[0-9]{5}(-[0-9]{4})?$)")
+        return zipCodeTestPredicate.evaluate(with: zipCodeTextField.text)
+    }
+    
+    @IBAction func checkZipCodeButtonTapped(_ sender: Any) {
+        if self.zipCodeTextField.text?.count == 5 && isZipCodeValid(text: self.zipCodeTextField.text!) == true
+        {
+            let alertController = UIAlertController(title: "Correct Zipcode", message: "Correct zipCode Input!", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        }
+            
+        else  {
+            let alertController = UIAlertController(title: "Incorrect Zipcode", message: "Invalid zipcode input!", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
