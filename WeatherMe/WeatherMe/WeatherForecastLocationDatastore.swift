a278ff97033163069ec46891bc7e741d1f759722
saraahmad1114 <asara1114@gmail.com>
2018-02-19 14:51:22 -0500

    fixed currentWEather json

--- WeatherMe/WeatherMe/WeatherForecastLocationDatastore.swift
+++ WeatherMe/WeatherMe/WeatherForecastLocationDatastore.swift
@@ -20,36 +20,34 @@ class WeatherForecastLocationDatastore{
         var newLng = -1 * lng
         
         DarkSkyAPIClient.getWeatherInformation(lat: lat, lng: newLng) { (darkSkyJson) in
-            
-            //print(darkSkyJson)
-            
+                        
             guard let jsonDictionary = darkSkyJson as? [String: Any] else {print("first level dictionary did not unwrap"); return}
             
-            print(jsonDictionary)
+            guard let currentDictionary = jsonDictionary["currently"] as? [String: Any] else{print("did not unwrap currentDictionary did not unwrap"); return}
             
-            guard let summary = jsonDictionary["summary"] as? String else {print("did not unwrap currentSummary"); return}
+            guard let summary = currentDictionary["summary"] as? String else {print("did not unwrap currentSummary"); return}
             
-            guard let icon = jsonDictionary["icon"] as? String else {print("did not unwrap currentIcon"); return}
+            guard let icon = currentDictionary["icon"] as? String else {print("did not unwrap currentIcon"); return}
             
-            guard let precipProb = jsonDictionary["precipProbability"] as? Double else{print("did not unwrap currentPrecipProbability"); return}
+            guard let precipProb = currentDictionary["precipProbability"] as? Double else{print("did not unwrap currentPrecipProbability"); return}
             
-            guard let temperature = jsonDictionary["temperature"] as? Double else{print("did not unwrap currentTemperature"); return}
+            guard let temperature = currentDictionary["temperature"] as? Double else{print("did not unwrap currentTemperature"); return}
             
-            guard let dewPoint = jsonDictionary["dewPoint"] as? Double else{print("did not unwrap currentDewPoint"); return}
+            guard let dewPoint = currentDictionary["dewPoint"] as? Double else{print("did not unwrap currentDewPoint"); return}
             
-            guard let humidity = jsonDictionary["humidity"] as? Double else{print("did not unwrap currentHumidity"); return}
+            guard let humidity = currentDictionary["humidity"] as? Double else{print("did not unwrap currentHumidity"); return}
             
-            guard let pressure = jsonDictionary["pressure"] as? Double else{print("did not unwrap currentPressue"); return}
+            guard let pressure = currentDictionary["pressure"] as? Double else{print("did not unwrap currentPressue"); return}
             
-            guard let windSpeed = jsonDictionary["windSpeed"] as? Double else{print("did not unwrap currentWindSpeed"); return}
+            guard let windSpeed = currentDictionary["windSpeed"] as? Double else{print("did not unwrap currentWindSpeed"); return}
             
-            guard let cloudCover = jsonDictionary["cloudCover"] as? Double else{print("did not unwrap currentCloudCover"); return}
+            guard let cloudCover = currentDictionary["cloudCover"] as? Double else{print("did not unwrap currentCloudCover"); return}
             
-            guard let uVIndex = jsonDictionary["uvIndex"] as? Double else{print("did not unwrap currentUVIndex"); return}
+            guard let uVIndex = currentDictionary["uvIndex"] as? Double else{print("did not unwrap currentUVIndex"); return}
             
-            guard let visibility = jsonDictionary["visibility"] as? Double else{print("did not unwrap currentVisibility"); return}
+            guard let visibility = currentDictionary["visibility"] as? Double else{print("did not unwrap currentVisibility"); return}
             
-            guard let ozone = jsonDictionary["ozone"] as? Double else{print("did not unwrap currentOzone"); return}
+            guard let ozone = currentDictionary["ozone"] as? Double else{print("did not unwrap currentOzone"); return}
             
             let currentWeatherForecastObj = CurrentWeather.init(currentSummary: summary, currentIcon: icon, currentPrecipProbability: precipProb, currentTemperature: temperature, currentDewPoint: dewPoint, currentHumidity: humidity, currentPressure: pressure, currentWindSpeed: windSpeed, currentCloudCover: cloudCover, currentUVIndex: uVIndex, currentVisibility: visibility, currentOzone: ozone)
             
