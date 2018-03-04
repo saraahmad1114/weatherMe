a8c75292625d9d42b3de66dd96ad25bd87a6f554
saraahmad1114 <asara1114@gmail.com>
2018-03-04 12:51:36 -0500

    almost done updating labels

--- WeatherMe/WeatherMe/CurrentWeatherViewController.swift
+++ WeatherMe/WeatherMe/CurrentWeatherViewController.swift
@@ -29,33 +29,20 @@ class CurrentWeatherViewController: UIViewController {
         super.viewDidLoad()
         createGradientLayer()
         if self.coordinateHolder != nil {
-            //core location
+            
             guard let unwrappedLat = self.coordinateHolder?.coordinate.latitude else {print("lat did not unwrap"); return}
             guard let unwrappedLng = self.coordinateHolder?.coordinate.longitude else{print("lng did not unwrap"); return}
             
             self.weatherStore.getWeatherForecastInformation(lat: unwrappedLat, lng: unwrappedLng, completion: { (current, hourly, daily) in
-                
+    
                 guard let location = self.weatherStore.currentWeatherArray.first?.timeZone else{print("location did not unwrap"); return}
                 guard let time = self.weatherStore.currentWeatherArray.first?.time else{print("time did not unwrap"); return}
-                
                 guard let summary = self.weatherStore.currentWeatherArray.first?.summary else{print("summary did not unwrap"); return}
-                
                 guard let icon = self.weatherStore.currentWeatherArray.first?.icon else{print("icon did not unwrap"); return}
-                
                 guard let temperature = self.weatherStore.currentWeatherArray.first?.temperature else{print("temperature did not unwrap"); return}
-                
                 guard let precip = self.weatherStore.currentWeatherArray.first?.precipProbability else{print("precipProbability did not unwrap"); return}
-                
                 guard let humidity = self.weatherStore.currentWeatherArray.first?.humidity else{print("humidity did not unwrap"); return}
-                
                 guard let windSpeed = self.weatherStore.currentWeatherArray.first?.windSpeed else{print("windspeed did not unwrap"); return}
-                
-                //let fullNameArr = fullName.components(separatedBy: " ")
-                
-                var neededDay = self.dayOfWeek(givenTime: time)
-                var neededHour = self.convertTimestampHour(givenTime: time)
-                var justHourString = neededHour.components(separatedBy: "at")
-                var takenHour = justHourString[1]
                 
                 OperationQueue.main.addOperation {
                     self.locationLabel.text = location
@@ -63,8 +50,8 @@ class CurrentWeatherViewController: UIViewController {
                     self.precipLabel.text = "\(String(Int(precip * 100))) %"
                     self.humidityLabel.text = "\(String(Int(humidity * 100))) %"
                     self.windSpeedLabel.text = "\(String(Int(windSpeed))) mph"
-                    self.dayLabel.text = neededDay
-                    self.hourLabel.text = takenHour
+                    self.dayLabel.text = self.dayOfWeek(givenTime: time)
+                    self.hourLabel.text = self.returnTimefrom(timeStamp: time)
                 }
             })
         }
@@ -73,12 +60,11 @@ class CurrentWeatherViewController: UIViewController {
             guard let unwrappedZipcode = self.zipCode else {print("did not unwrap zipcode"); return}
             self.coordinateStore.getUserCoordintes(zipcode: unwrappedZipcode, completion: { (coordinatesJson) in
                 
-                //coordinate components
                 guard let lat = self.coordinateStore.locationCoordinates.first?.latitude else{print("did not unwrap lat"); return}
                 guard let lng = self.coordinateStore.locationCoordinates.first?.longitude else{print("did not unwrap lng"); return}
 
                 self.weatherStore.getWeatherForecastInformation(lat: lat, lng: lng, completion: { (current, hourly, daily) in
-                    //label components
+                    
                     guard let location = self.weatherStore.currentWeatherArray.first?.timeZone else{print("location did not unwrap"); return}
                     guard let time = self.weatherStore.currentWeatherArray.first?.time else{print("time did not unwrap"); return}
                     guard let summary = self.weatherStore.currentWeatherArray.first?.summary else{print("summary did not unwrap"); return}
@@ -87,11 +73,6 @@ class CurrentWeatherViewController: UIViewController {
                     guard let precip = self.weatherStore.currentWeatherArray.first?.precipProbability else{print("precipProbability did not unwrap"); return}
                     guard let humidity = self.weatherStore.currentWeatherArray.first?.humidity else{print("humidity did not unwrap"); return}
                     guard let windSpeed = self.weatherStore.currentWeatherArray.first?.windSpeed else{print("windspeed did not unwrap"); return}
-                    
-                    var neededDay = self.dayOfWeek(givenTime: time)
-                    var neededHour = self.convertTimestampHour(givenTime: time)
-                    var justHourString = neededHour.components(separatedBy: "at")
-                    var takenHour = justHourString[1]
 
                     OperationQueue.main.addOperation {
               
@@ -100,8 +81,8 @@ class CurrentWeatherViewController: UIViewController {
                         self.precipLabel.text = "\(String(Int(precip * 100))) %"
                         self.humidityLabel.text = "\(String(Int(humidity * 100))) %"
                         self.windSpeedLabel.text = "\(String(Int(windSpeed))) mph"
-                        self.dayLabel.text = neededDay
-                        self.hourLabel.text = takenHour
+                        self.dayLabel.text = self.dayOfWeek(givenTime: time)
+                        self.hourLabel.text = self.returnTimefrom(timeStamp: time)
  
                     }
                    
@@ -141,6 +122,14 @@ class CurrentWeatherViewController: UIViewController {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE"
         return dateFormatter.string(from: date).capitalized
+    }
+    
+    //returns the time from given string
+    func returnTimefrom (timeStamp: Double) -> String{
+        var neededHour = self.convertTimestampHour(givenTime: timeStamp)
+        var justHourString = neededHour.components(separatedBy: "at")
+        var takenHour = justHourString[1]
+        return takenHour
     }
 
     override func didReceiveMemoryWarning() {
