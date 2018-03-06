2b2faa53c059aa397487fefeb9d4769d8d51c19f
saraahmad1114 <asara1114@gmail.com>
2018-03-06 17:59:42 -0500

    added view

--- WeatherMe/WeatherMe/CurrentWeatherViewController.swift
+++ WeatherMe/WeatherMe/CurrentWeatherViewController.swift
@@ -23,6 +23,8 @@ class CurrentWeatherViewController: UIViewController, UICollectionViewDelegate, 
     @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var dailyWeatherCollectionView: UICollectionView!
+    @IBOutlet weak var graphView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ WeatherMe/WeatherMe/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  WeatherMe
+//
+//  Created by Sara Ahmad on 3/6/18.
+//  Copyright © 2018 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
