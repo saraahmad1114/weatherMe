fe680e39ce3908c3acb0fb15981fac84261671b1
saraahmad1114 <asara1114@gmail.com>
2018-03-07 15:35:03 -0500

    created tableviewcell clas

---
+++ WeatherMe/WeatherMe/HourlyWeatherTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  HourlyWeatherTableViewCell.swift
+//  WeatherMe
+//
+//  Created by Sara Ahmad on 3/7/18.
+//  Copyright © 2018 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class HourlyWeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
