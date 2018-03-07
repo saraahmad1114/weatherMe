0355593852462d97daa226eac87c8030d781686f
saraahmad1114 <asara1114@gmail.com>
2018-03-07 15:14:49 -0500

    fixed hourly class

--- WeatherMe/WeatherMe/HourlyWeather.swift
+++ WeatherMe/WeatherMe/HourlyWeather.swift
@@ -10,14 +10,17 @@ class HourlyWeather {
 
     var hourlyTime: Double?
     var hourlyTemperature: Double?
+    var hourlyIcon: String?
     
     init(jsonDictionary: [String: Any]) {
         guard
             let time = jsonDictionary["time"] as? Double,
-            let temperature = jsonDictionary["temperature"] as? Double
+            let temperature = jsonDictionary["temperature"] as? Double,
+            let icon = jsonDictionary["icon"] as? String
             else {print("did not unwrap hourlyWeather"); return}
         self.hourlyTime = time
         self.hourlyTemperature = temperature
+        self.hourlyIcon = icon
     }
     
 }
