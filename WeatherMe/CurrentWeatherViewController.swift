d253ac899e580ee7493f1661d5324927383fab89
saraahmad1114 <asara1114@gmail.com>
2018-03-16 19:56:07 -0400

    chjangs

--- WeatherMe/CurrentWeatherViewController.swift
+++ WeatherMe/CurrentWeatherViewController.swift
@@ -13,17 +13,17 @@ class CurrentWeatherViewController: UIViewController, UICollectionViewDelegate, 
     let coordinateStore = CoordinatesDatastore.sharedInstance
     let weatherStore = WeatherForecastLocationDatastore.sharedInstance
     
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var dayLabel: UILabel!
-    @IBOutlet weak var hourLabel: UILabel!
-    @IBOutlet weak var summaryLabel: UILabel!
-    @IBOutlet weak var precipLabel: UILabel!
-    @IBOutlet weak var humidityLabel: UILabel!
-    @IBOutlet weak var windSpeedLabel: UILabel!
-    @IBOutlet weak var iconImage: UIImageView!
-    @IBOutlet weak var tempLabel: UILabel!
-    @IBOutlet weak var dailyWeatherCollectionView: UICollectionView!
-    @IBOutlet weak var hourlyWeatherTableview: UITableView!
+//    @IBOutlet weak var locationLabel: UILabel!
+//    @IBOutlet weak var dayLabel: UILabel!
+//    @IBOutlet weak var hourLabel: UILabel!
+//    @IBOutlet weak var summaryLabel: UILabel!
+//    @IBOutlet weak var precipLabel: UILabel!
+//    @IBOutlet weak var humidityLabel: UILabel!
+//    @IBOutlet weak var windSpeedLabel: UILabel!
+//    @IBOutlet weak var iconImage: UIImageView!
+//    @IBOutlet weak var tempLabel: UILabel!
+//    @IBOutlet weak var dailyWeatherCollectionView: UICollectionView!
+//    @IBOutlet weak var hourlyWeatherTableview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,18 +40,18 @@ class CurrentWeatherViewController: UIViewController, UICollectionViewDelegate, 
                 guard let humidity = self.weatherStore.currentWeatherArray.first?.humidity else{print("humidity did not unwrap"); return}
                 guard let windSpeed = self.weatherStore.currentWeatherArray.first?.windSpeed else{print("windspeed did not unwrap"); return}
                 OperationQueue.main.addOperation {
-                    self.locationLabel.text = self.returnLocationString(location: location)
-                    self.summaryLabel.text = summary
-                    self.precipLabel.text = "\(String(Int(precip * 100))) %"
-                    self.humidityLabel.text = "\(String(Int(humidity * 100))) %"
-                    self.windSpeedLabel?.text = "\(String(Int(windSpeed))) mph"
-                    self.dayLabel.text = self.dayOfWeek(givenTime: time)
-                    self.hourLabel.text = self.returnTimefrom(timeStamp: time)
-                    self.tempLabel.text = "\(String(Int(temperature))) F"
-                    self.returnImageForIcon(icon: icon)
-                    
-                    self.dailyWeatherCollectionView.reloadData()
-                    self.hourlyWeatherTableview.reloadData()
+//                    self.locationLabel.text = self.returnLocationString(location: location)
+//                    self.summaryLabel.text = summary
+//                    self.precipLabel.text = "\(String(Int(precip * 100))) %"
+//                    self.humidityLabel.text = "\(String(Int(humidity * 100))) %"
+//                    self.windSpeedLabel?.text = "\(String(Int(windSpeed))) mph"
+//                    self.dayLabel.text = self.dayOfWeek(givenTime: time)
+//                    self.hourLabel.text = self.returnTimefrom(timeStamp: time)
+//                    self.tempLabel.text = "\(String(Int(temperature))) F"
+//                    self.returnImageForIcon(icon: icon)
+//
+//                    self.dailyWeatherCollectionView.reloadData()
+//                    self.hourlyWeatherTableview.reloadData()
                 }
             })
         }
@@ -70,18 +70,18 @@ class CurrentWeatherViewController: UIViewController, UICollectionViewDelegate, 
                     guard let humidity = self.weatherStore.currentWeatherArray.first?.humidity else{print("humidity did not unwrap"); return}
                     guard let windSpeed = self.weatherStore.currentWeatherArray.first?.windSpeed else{print("windspeed did not unwrap"); return}
                     OperationQueue.main.addOperation {
-                        self.locationLabel.text = self.returnLocationString(location: location)
-                        self.summaryLabel.text = summary
-                        self.precipLabel.text = "\(String(Int(precip * 100))) %"
-                        self.humidityLabel.text = "\(String(Int(humidity * 100))) %"
-                        self.windSpeedLabel.text = "\(String(Int(windSpeed))) mph"
-                        self.dayLabel.text = self.dayOfWeek(givenTime: time)
-                        self.hourLabel.text = self.returnTimefrom(timeStamp: time)
-                        self.tempLabel.text = "\(String(Int(temperature))) F"
-                        self.returnImageForIcon(icon: icon)
-                        
-                        self.dailyWeatherCollectionView.reloadData()
-                        self.hourlyWeatherTableview.reloadData()
+//                        self.locationLabel.text = self.returnLocationString(location: location)
+//                        self.summaryLabel.text = summary
+//                        self.precipLabel.text = "\(String(Int(precip * 100))) %"
+//                        self.humidityLabel.text = "\(String(Int(humidity * 100))) %"
+//                        self.windSpeedLabel.text = "\(String(Int(windSpeed))) mph"
+//                        self.dayLabel.text = self.dayOfWeek(givenTime: time)
+//                        self.hourLabel.text = self.returnTimefrom(timeStamp: time)
+//                        self.tempLabel.text = "\(String(Int(temperature))) F"
+//                        self.returnImageForIcon(icon: icon)
+//
+//                        self.dailyWeatherCollectionView.reloadData()
+//                        self.hourlyWeatherTableview.reloadData()
                     }
                 })
             })
@@ -223,6 +223,7 @@ class CurrentWeatherViewController: UIViewController, UICollectionViewDelegate, 
         return localDate
     }
     
+    //returns day of the week only
     func dayOfWeek(givenTime: Double) -> String {
         let date = Date(timeIntervalSince1970: givenTime)
         let dateFormatter = DateFormatter()
@@ -237,52 +238,54 @@ class CurrentWeatherViewController: UIViewController, UICollectionViewDelegate, 
         return takenHour
     }
     
+    
+    
     func returnLocationString (location: String) -> String{
         var locationString = location.components(separatedBy: "/")
         return locationString[1].replacingOccurrences(of: "_", with: " ")
     }
     
-    func returnImageForIcon (icon: String){
-        if icon == "clear-day"{
-            self.iconImage.image = UIImage(named:"clear-day")
-        }
-        else if icon == "clear-night"{
-            self.iconImage.image = UIImage(named: "clear-night")
-        }
-        else if icon == "cloudy"{
-            self.iconImage.image = UIImage(named: "cloudy")
-        }
-        else if icon == "fog"{
-           self.iconImage.image = UIImage(named: "fog")
-        }
-        else if icon == "hail"{
-            self.iconImage.image = UIImage(named:"hail")
-        }
-        else if icon == "partly-cloudy-day"{
-            self.iconImage.image = UIImage(named:"partly-cloudy-day")
-        }
-        else if icon == "partly-cloudy-night"{
-            self.iconImage.image = UIImage(named:"partly-cloudy-night")
-        }
-        else if icon == "rain"{
-            self.iconImage.image = UIImage(named:"rain")
-        }
-        else if icon == "sleet"{
-            self.iconImage.image = UIImage(named:"sleet")
-        }
-        else if icon == "snow"{
-            self.iconImage.image = UIImage(named:"snow")
-        }
-        else if icon == "thunderstorm"{
-            self.iconImage.image = UIImage(named:"thunderstorm")
-        }
-        else if icon == "tornado"{
-            self.iconImage.image = UIImage(named:"tornado")
-        }
-        else if icon == "wind"{
-            self.iconImage.image = UIImage(named:"wind")
-        }
-    }
+//    func returnImageForIcon (icon: String){
+//        if icon == "clear-day"{
+//            self.iconImage.image = UIImage(named:"clear-day")
+//        }
+//        else if icon == "clear-night"{
+//            self.iconImage.image = UIImage(named: "clear-night")
+//        }
+//        else if icon == "cloudy"{
+//            self.iconImage.image = UIImage(named: "cloudy")
+//        }
+//        else if icon == "fog"{
+//           self.iconImage.image = UIImage(named: "fog")
+//        }
+//        else if icon == "hail"{
+//            self.iconImage.image = UIImage(named:"hail")
+//        }
+//        else if icon == "partly-cloudy-day"{
+//            self.iconImage.image = UIImage(named:"partly-cloudy-day")
+//        }
+//        else if icon == "partly-cloudy-night"{
+//            self.iconImage.image = UIImage(named:"partly-cloudy-night")
+//        }
+//        else if icon == "rain"{
+//            self.iconImage.image = UIImage(named:"rain")
+//        }
+//        else if icon == "sleet"{
+//            self.iconImage.image = UIImage(named:"sleet")
+//        }
+//        else if icon == "snow"{
+//            self.iconImage.image = UIImage(named:"snow")
+//        }
+//        else if icon == "thunderstorm"{
+//            self.iconImage.image = UIImage(named:"thunderstorm")
+//        }
+//        else if icon == "tornado"{
+//            self.iconImage.image = UIImage(named:"tornado")
+//        }
+//        else if icon == "wind"{
+//            self.iconImage.image = UIImage(named:"wind")
+//        }
+//    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
