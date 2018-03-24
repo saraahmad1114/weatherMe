ec224ee61fc7829edeb253c355f4939d775b656c
saraahmad1114 <asara1114@gmail.com>
2018-03-24 13:04:13 -0400

    changes

--- WeatherMe/ViewController.swift
+++ WeatherMe/ViewController.swift
@@ -5,7 +5,7 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager?
     var currentLocation: CLLocation?
@@ -15,17 +15,18 @@ class ViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("*************************")
-        self.locationManager = CLLocationManager()
-        self.locationManager?.delegate = self as? CLLocationManagerDelegate
-        self.locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        self.locationManager?.requestWhenInUseAuthorization()
-        print("*************************")
     }
+
     @IBAction func findMyLocationButtonTapped(_ sender: Any) {
         print("this button was pressed")
+        self.locationManager = CLLocationManager()
+        self.locationManager?.delegate = self
+        self.locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        self.locationManager?.requestWhenInUseAuthorization()
+        self.locationManager?.requestLocation()
     }
     
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "weatherSegue" {
         if let destinationVC = segue.destination as? WeatherForecastViewController {
@@ -40,29 +41,136 @@ class ViewController: UIViewController{
                 }
             }
         }
-    }
     
-extension ViewController : CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            if self.currentLocation == nil {
-                if let personCoordinates = locations.first{
-                    self.currentLocation = personCoordinates
-                    print("LOOK HERE FOR COORDINATES")
-                    print(personCoordinates.coordinate.latitude)
-                    print(personCoordinates.coordinate.longitude)
-                    print("LOOK HERE FOR COORDINATES")
-                }
+        if self.currentLocation == nil {
+            if let personCoordinates = locations.first{
+                self.currentLocation = personCoordinates
+                print("LOOK HERE FOR COORDINATES")
+                print(personCoordinates.coordinate.latitude)
+                print(personCoordinates.coordinate.longitude)
+                print("LOOK HERE FOR COORDINATES")
             }
         }
-
+    }
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
             self.locationManager?.startUpdatingLocation()
         }
         else if status == .notDetermined || status == .denied || status == .restricted {
             self.locationManager?.requestWhenInUseAuthorization()
-            }
         }
     }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("core location did not work: \(error)")
+    }
+    
+    }
+    
+//extension ViewController : CLLocationManagerDelegate {
 
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//            if self.currentLocation == nil {
+//                if let personCoordinates = locations.first{
+//                    self.currentLocation = personCoordinates
+//                    print("LOOK HERE FOR COORDINATES")
+//                    print(personCoordinates.coordinate.latitude)
+//                    print(personCoordinates.coordinate.longitude)
+//                    print("LOOK HERE FOR COORDINATES")
+//                }
+//            }
+//        }
+//
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        if status == .authorizedWhenInUse {
+//            self.locationManager?.startUpdatingLocation()
+//        }
+//        else if status == .notDetermined || status == .denied || status == .restricted {
+//            self.locationManager?.requestWhenInUseAuthorization()
+//            }
+//        }
+//
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//        print("core location did not work")
+//    }
+//}
+
+//==========================
+////
+////  ViewController.swift
+////  WeatherMe
+//
+//import UIKit
+//import CoreLocation
+//
+//class ViewController: UIViewController{
+//
+//    var locationManager: CLLocationManager?
+//    var currentLocation: CLLocation?
+//    @IBOutlet weak var zipCodeTextField: UITextField!
+//    let store = WeatherForecastLocationDatastore.sharedInstance
+//    var isCoreLocationEnabled: Bool?
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        print("*************************")
+//        self.locationManager = CLLocationManager()
+//        self.locationManager?.delegate = self
+//        self.locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+//        self.locationManager?.requestWhenInUseAuthorization()
+//        self.locationManager?.requestLocation()
+//        self.locationManager?.startUpdatingLocation()
+//        print("*************************")
+//    }
+//    @IBAction func findMyLocationButtonTapped(_ sender: Any) {
+//        print("this button was pressed")
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "weatherSegue" {
+//            if let destinationVC = segue.destination as? WeatherForecastViewController {
+//                if self.currentLocation != nil {
+//                    guard let latestCoordinates = self.currentLocation else {print("latestCoordinates did not unwrap"); return}
+//                    destinationVC.coordinateHolder = latestCoordinates
+//                }
+//                else if self.zipCodeTextField.text != nil{
+//                    guard let neededZipcode = self.zipCodeTextField.text else {print("neededZipcode did not unwrap"); return}
+//                    destinationVC.zipCode = neededZipcode
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//extension ViewController : CLLocationManagerDelegate {
+//
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        if self.currentLocation == nil {
+//            if let personCoordinates = locations.first{
+//                self.currentLocation = personCoordinates
+//                print("LOOK HERE FOR COORDINATES")
+//                print(personCoordinates.coordinate.latitude)
+//                print(personCoordinates.coordinate.longitude)
+//                print("LOOK HERE FOR COORDINATES")
+//            }
+//        }
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        if status == .authorizedWhenInUse {
+//            self.locationManager?.startUpdatingLocation()
+//        }
+//        else if status == .notDetermined || status == .denied || status == .restricted {
+//            self.locationManager?.requestWhenInUseAuthorization()
+//        }
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//        print("core location did not work")
+//    }
+//}
+//
+//
 
