1ffa47c128faed22bd61addf614acb82e20e8189
saraahmad1114 <asara1114@gmail.com>
2018-03-28 15:15:31 -0400

    need to fix coordinates for segue

--- WeatherMe/Controller/ViewController.swift
+++ WeatherMe/Controller/ViewController.swift
@@ -66,10 +66,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if self.currentLocation == nil {
             if let personCoordinates = locations.first{
                 self.currentLocation = personCoordinates
-                print("LOOK HERE FOR COORDINATES")
                 print(personCoordinates.coordinate.latitude)
                 print(personCoordinates.coordinate.longitude)
-                print("LOOK HERE FOR COORDINATES")
             }
         }
     }
