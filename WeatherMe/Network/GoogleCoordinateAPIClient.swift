954e3ba614e8bccb286450a6a761e6eae115639b
saraahmad1114 <asara1114@gmail.com>
2018-04-09 20:03:19 -0400

    clean u

--- WeatherMe/Network/GoogleCoordinateAPIClient.swift
+++ WeatherMe/Network/GoogleCoordinateAPIClient.swift
@@ -13,29 +13,17 @@ import Foundation
 class GoogleCoordinateAPIClient {
     
     class func getCoordinateInformation (zipCode: String, completion:@escaping([String: Any])->()) throws {
-        
         let zipCode = zipCode.replacingOccurrences(of: " ", with: "+")
-
         var jsonDictionary = [String: Any]()
-
         let url = "https://maps.googleapis.com/maps/api/geocode/json?address=\(zipCode)&key=\(Secrets.googleCoordinateApiKey)"
-        
         let convertedUrl = URL(string: url)
-        
         guard let unwrappedConvertedUrl = convertedUrl else {print("unwrappedConvertedUrl did not unwrap"); return}
-        
         let request = URLRequest(url: unwrappedConvertedUrl)
-        
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
             guard let unwrappedData = data else {print("unwrappedData did not unwrap"); return}
-            
             do{
-            
             let json = try JSONSerialization.jsonObject(with: unwrappedData, options: []) as! [String: Any]
-                        
             jsonDictionary = json
-            
             completion(jsonDictionary)
             }
             catch let error{
@@ -53,7 +41,7 @@ class GoogleCoordinateAPIClient {
         guard let unwrappedConvertedUrl = convertedUrl else {print("unwrappedConvertedUrl did not unwrap"); return}
         let request = URLRequest(url: unwrappedConvertedUrl)
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            //guard let unwrappedData = data else {print("unwrappedData did not unwrap"); return}
+            guard let unwrappedData = data else {print("unwrappedData did not unwrap"); return}
             guard let httpResponse = response as? HTTPURLResponse else{print("httpResponse did not unwrap"); return}
             if httpResponse.statusCode == 200 {
                 isAddressValid = true
