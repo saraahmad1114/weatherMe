a1a6e3de425a10fd9fb1249923ff886139b8e9e7
saraahmad1114 <asara1114@gmail.com>
2018-04-10 15:18:40 -0400

    comments for functions

--- WeatherMe/Network/CoordinatesDatastore.swift
+++ WeatherMe/Network/CoordinatesDatastore.swift
@@ -17,25 +17,18 @@ class CoordinatesDatastore {
     
     var locationCoordinates = [Coordinates]()
     
+    //Getting JSON and creating the Swift Object 
     func getUserCoordintes (zipcode: String, completion:@escaping ([Coordinates]) -> ()) throws {
         do {
             try GoogleCoordinateAPIClient.getCoordinateInformation(zipCode: zipcode) { (googleAPICoordinatesJson) in
-            
             guard let secondLevelArray = googleAPICoordinatesJson["results"] as? Array<Any> else {print("did not unwrap at the second level"); return}
-            
             guard let firstElementFromArray = secondLevelArray[0] as? [String : Any] else {print("did not unwrap at the third level"); return}
-            
             guard let geometryDictionary = firstElementFromArray["geometry"] as? [String: Any] else {print("did not unwrap at the fourth level"); return}
-            
             guard let locationDictionary = geometryDictionary["location"] as? [String: Any] else {print("did not unwrap at the fifth level"); return}
-            
             guard let locationLat = locationDictionary["lat"] as? Double else {print("did not unwrap latitude"); return}
             guard let locationLng = locationDictionary["lng"] as? Double else {print("did not unwrap longitude"); return}
-            
             let coordinatesObject = Coordinates.init(latitude: locationLat, longitude: locationLng)
-                        
             self.locationCoordinates.append(coordinatesObject)
-                
             completion(self.locationCoordinates)
             }
         }
