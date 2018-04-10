0461d27df189f141d5cf1b5a2dc90119e302ef68
saraahmad1114 <asara1114@gmail.com>
2018-04-10 15:17:54 -0400

    adding comments

--- WeatherMe/Network/DarkSkyAPIClient .swift
+++ WeatherMe/Network/DarkSkyAPIClient .swift
@@ -11,37 +11,24 @@ import Foundation
 
 class DarkSkyAPIClient {
     
+    //Function to get JSON
     class func getWeatherInformation(lat: Double, lng: Double, completion:@escaping([String: Any])->()) throws {
-        
         var jsonDictionary = [String: Any]()
-
         let url = "https://api.darksky.net/forecast/\(Secrets.darkSkyApiKey)/\(lat),\(-lng)"
-        
         let convertedUrl = URL(string: url)
-        
         guard let unwrappedUrl = convertedUrl else {print("unwrappedUrl did not unwrap"); return}
-        
         let request = URLRequest(url: unwrappedUrl)
-        
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
             guard let unwrappedData = data else {print("data did not unwrap"); return}
-            
             do{
-            
             let jsonResponseDictionary = try JSONSerialization.jsonObject(with: unwrappedData, options: []) as! [String: Any]
-                
             jsonDictionary = jsonResponseDictionary
-
             completion(jsonDictionary)
             }
             catch let error{
                 print("error getting back json: \(error.localizedDescription)")
             }
         }
-        
         task.resume()
-    
     }
-    
 }
