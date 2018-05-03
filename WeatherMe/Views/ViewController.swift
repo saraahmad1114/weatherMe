26c5a961e17bb727db709e06af3319859272e539
saraahmad1114 <asara1114@gmail.com>
2018-05-03 16:22:07 -0400

    added activity indicator

--- WeatherMe/Views/ViewController.swift
+++ WeatherMe/Views/ViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 import CoreLocation
 
-//MARK: TO DO LIST:
-//1. implement an activity indicator, just to provide enough time before you segue over - core location
-//2. implement an activity indicator, just to provide enough time before you segue over - user input
-
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager?
@@ -21,7 +17,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var userInputLocationSuccess: Bool?
     var coreLocationSuccess: Bool?
     @IBOutlet weak var locationProgressSpinner: UIActivityIndicatorView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,13 +40,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBAction func goButtonTapped(_ sender: Any) {
         if let userText = self.zipCodeTextField.text{
             GoogleCoordinateAPIClient.isAddressValid(zipCode: userText) { (boolValue) in
-                //activity indicator needs to go here with a while loop, so that it proceeds over, showing the user that the location is being found.
                 if boolValue == true {
                     self.userInputLocationSuccess = true
-                    self.performSegue(withIdentifier: "goButtonSegue", sender: self)
+                    OperationQueue.main.addOperation {
+                        self.performSegue(withIdentifier: "goButtonSegue", sender: self)
+                        self.locationProgressSpinner.stopAnimating()
+                    }
                 }
                 else if boolValue == false {
                     self.userInputLocationSuccess = false
+                    self.locationProgressSpinner.hidesWhenStopped = true 
+                    self.locationProgressSpinner.startAnimating()
                     self.presentAlert("Invalid Input", message: "Please re-enter valid input", cancelTitle: "OK")
                     self.shouldPerformSegue(withIdentifier: "goButtonSegue", sender: self)
                 }
